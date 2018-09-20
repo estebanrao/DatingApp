856fe2eef2f6d9ce2dbe8c5a761b57f148ff96d5
Esteban Rao <estebanrao@gmail.com>
2018-09-20 18:35:26 -0300

    End of section 9

--- DatingApp.API/Dtos/UserForDetailsDto.cs
+++ DatingApp.API/Dtos/UserForDetailsDto.cs
@@ -13,7 +13,7 @@ namespace DatingApp.API.Dtos
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
-        public string Intriduction { get; set; }
+        public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
