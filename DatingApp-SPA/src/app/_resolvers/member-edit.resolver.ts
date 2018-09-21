923d8c71adc161af676c108a676c4f4728319afb
Esteban Rao <estebanrao@gmail.com>
2018-09-20 23:23:13 -0300

    End of section 10

--- DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -5,22 +5,26 @@ import { UserService } from '../_services/user.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
-export class MemberDetailsResolver implements Resolve<User> {
+export class MemberEditResolver implements Resolve<User> {
     constructor(
         private userService: UserService,
         private router: Router,
+        private authService: AuthService,
         private alertify: AlertifyService
     ) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User> {
-        return this.userService.getUser(route.params['id']).pipe(
-            catchError(error => {
-                this.alertify.error('Problem retreiving data');
-                this.router.navigate(['/members']);
-                return of(null);
-            })
-        );
+        return this.userService
+            .getUser(this.authService.decodedToken.nameid)
+            .pipe(
+                catchError(error => {
+                    this.alertify.error('Problem retreiving your data');
+                    this.router.navigate(['/members']);
+                    return of(null);
+                })
+            );
     }
 }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -28,6 +28,7 @@ namespace DatingApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailsDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
